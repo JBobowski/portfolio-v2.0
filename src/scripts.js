7c3a517ad4a8935f28e732948413c8e5f04486dc
BigYoplait <48740378+BigYoplait@users.noreply.github.com>
2022-12-08 21:13:37 -0500

    Updated Portfolio 2.0
    Made small adjustments to the layout

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,14 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
+const navLinkAbout = document.getElementById('nav-link-about')
+const navLinkProj = document.getElementById('nav-link-proj')
+const navLinkCont = document.getElementById('nav-link-cont')
 
 function toggleButton() {
     navList.classList.toggle('show')
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+navLinkAbout.addEventListener('click', toggleButton)
+navLinkProj.addEventListener('click', toggleButton)
+navLinkCont.addEventListener('click', toggleButton)
